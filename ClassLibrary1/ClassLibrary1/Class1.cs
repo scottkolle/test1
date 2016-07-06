1448c4fa19ddc0a2510c0e6567e4d928fdd690bd
scottkolle <scottolle@altacimas.com>
2016-07-06 14:30:01 -0600

    Revert "oops found another bug"
    This reverts commit 3102c2b3fb3b755a31a3e116106eebf47e247540.

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -17,11 +17,9 @@ namespace ClassLibrary1
 // commit 4
 // commit 5
 // commit 6
-// commit 7 try to add to this and conflict and just for the heck of it make it better
+// commit 7
 // commit 8
 // commit 9
-// commit 10  some additional changes to commit 10 and a little fix here
+// commit 10
 // commit 11
 // commit 12
-// commit 13 something totally new
-} // oops found another bug
