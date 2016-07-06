846f22bbe337ff28133639e4aee06c10ac99a9be
scottkolle <scottolle@altacimas.com>
2016-07-06 10:25:42 -0600

    merged in bug fix changes

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -8,6 +8,6 @@ namespace ClassLibrary1
 {
     public class Class1
     {// test comment
-    }// more test comments
-}
+    }// more test comments and fix bug
+} // oops found another bug
 //added new feature
