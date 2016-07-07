7e167072b232eba203731780a372a95663a2b8c9
scottkolle <scottolle@altacimas.com>
2016-07-06 19:31:22 -0600

    CLR Change

---
+++ OracleCLRProject1/OracleCLRProject1/Class1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace OracleCLRProject1
+{
+    public class Class1
+    {
+        public static void StoredProcedure1()
+        {// test oracle CLR change
+        }
+    }
+}
+// and another change
